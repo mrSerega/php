9123c0855456b888f01d03e7695d2f8ca1061fb2
mrSerega <sega00796@mail.ru>
2016-04-24 16:05:10 +0300

    some files 1605

---
+++ tinymvc.ru/application/views/template_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Main</title>
+</head>
+<body>
+    <?php include 'application/views/'.$content_view; ?>
+</body>
+</html>
+
+<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+<script src="js/jquery-1.6.2.js" type="text/javascript"></script>
